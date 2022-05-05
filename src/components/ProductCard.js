fa5bfce3a56ed4babae3c9a032989278198bc8e7
srosenberg74 <srosenberg@alphaworks.tech>
2022-05-05 15:31:31 -0700

    style(product card): alt weird bg style

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -5,14 +5,23 @@ import { Link } from "react-router-dom";
 function ProductCard({ title, image, price, description, id }) {
   return (
     <div className="product-card">
-      <div className="pc-inner-1"> </div>
-      <div className="pc-inner-2"> </div>
-      <div className="pc-inner-3"></div>
-      <div className="pc-inner-4"></div>
-      <div className="pc-inner-5"> </div>
-      <div className="pc-inner-6"> </div>
-      <div className="pc-inner-7"> </div>
-      <div className="pc-inner-8"></div>
+      <div className="pc-inner-1">
+      <div className="pc-inner-2">
+      <div className="pc-inner-3">
+      <div className="pc-inner-4">
+      <div className="pc-inner-5">
+      <div className="pc-inner-6">
+      <div className="pc-inner-7">
+      <div className="pc-inner-8">
+     
+      </div>
+      </div>
+      </div>
+      </div>
+      </div>
+      </div>
+      </div>
+      </div>
       <div className="pc">
       <Link to={`product/${id}`}>
         <h4>{title}</h4>
